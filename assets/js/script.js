c02699bb8c660f8128f76c1955b2b20be6e72308
ccorreas <correasberenguer@gmail.com>
2024-04-04 21:31:59 +0200

    Init, animate, render functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,56 @@
-console.log("Initial Script");
 import * as THREE from "three";
+import Stats from "three/addons/libs/stats.module.js";
+import { GUI } from "three/addons/libs/lil-gui.module.min.js";
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+
+let camera, scene, renderer, controls, stats;
+
+let mesh;
+
+const amount = parseInt(window.location.search.slice(1)) || 10;
+const count = Math.pow(amount, 3);
+
+const raycaster = new THREE.Raycaster();
+const mouse = new THREE.Vector2(1, 1);
+
+const color = new THREE.Color();
+const white = new THREE.Color().setHex(0xffffff);
+
+const init = () => {
+    camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 0.1, 1000);
+    camera.position.set(amount, amount, amount);
+    camera.lookAt(0, 0, 0);
+
+    scene = new THREE.Scene();
+
+    const light = new THREE.HemisphereLight(0xffffff, 0x888888, 3);
+    light.position.set(0, 1, 0);
+    scene.add(light);
+
+    const geometry = new THREE.IcosahedronGeometry(0.5, 3);
+    const material = new THREE.MeshPhongMaterial({ color: 0xffffff });
+
+    mesh = new THREE.InstancedMesh(geometry, material, count);
+
+    scene.add(mesh);
+
+    renderer = new THREE.WebGLRenderer({ antialias: true });
+    renderer.setPixelRatio(window.devicePixelRatio);
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(renderer.domElement);
+};
+
+const render = () => {
+    renderer.render(scene, camera);
+};
+
+const animate = () => {
+    requestAnimationFrame(animate);
+    render();
+};
+
+
+
+init();
+animate();
 
